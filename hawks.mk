33d2cc7e67618f853324b14159d17c2af8ca6534
ticktechman <ticktechman@gmail.com>
2026-09-06 13:54:06 +0800

    style: hawks.mk

--- hawks.mk
+++ hawks.mk
@@ -13,6 +13,7 @@ isa_dir     := isa
 rv64_suites := ui um ua uf ud uc mi si
 elf_dumps   := $(foreach b,$(rv64_suites),$(wildcard $(isa_dir)/rv64$(b)-p-*.dump))
 rv64_hex    := $(patsubst %.dump,$(hex_dir)/%.hex,$(notdir $(elf_dumps)))
+rv_objcpy   := riscv-none-elf-objcopy -O verilog --verilog-data-width=1 --no-change-warnings
 
 all: $(rv64_hex)
 
@@ -21,8 +22,8 @@ $(hex_dir)/%.hex:$(isa_dir)/%
 	@$(eval hex_data := $(@:.hex=.data))
 	@[[ -d $(hex_dir) ]] || mkdir $(hex_dir)
 	@cp $< $(hex_dir)/
-	@riscv-none-elf-objcopy -O verilog --verilog-data-width=1 -j .text.init --change-section-address .text.init=0 $< $@ >/dev/null
-	@riscv-none-elf-objcopy -O verilog --verilog-data-width=1 -j .data --change-section-address .data=0 --no-change-warnings $< $(hex_data) >/dev/null
+	@$(rv_objcpy) -j .text.init --change-section-address .text.init=0 $< $@ >/dev/null
+	@$(rv_objcpy) -j .data --change-section-address .data=0 $< $(hex_data) >/dev/null
 	@[[ -s $(hex_data) ]] || rm -f $(hex_data)
 
 clean:
